b4ac39cdd6391feee7deed84fc875c349874964b
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-04 10:20:34 -0400

    first commit

---
+++ src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -0,0 +1,20 @@
+package com.simon.curso.springboot.jpa.springboot_jpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+	}
+
+}
