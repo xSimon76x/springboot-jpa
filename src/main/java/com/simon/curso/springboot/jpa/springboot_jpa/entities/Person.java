878e56b7650027fe73f9654d8a4b7a2917355cef
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-05 09:58:06 -0400

    Annadiendo la clase entity

---
+++ src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/entities/Person.java
@@ -0,0 +1,70 @@
+package com.simon.curso.springboot.jpa.springboot_jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="persons")//Esto se podria omitir si la clase se llama igual a la tabla en el SQL
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //En estos campos, no es necesario usar el column, si se llaman igual a las columnas de la tabla SQL
+    private String name;
+    private String lastname;
+
+    @Column(name = "programming_language")
+    private String programmingLanguage;
+
+    public Person() {}
+    
+    public Person(Long id, String name, String lastname, String programmingLanguage) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+        this.programmingLanguage = programmingLanguage;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + ", lastname=" + lastname + ", programmingLanguage="
+                + programmingLanguage + ", toString()=" + super.toString() + "]";
+    }
+}
