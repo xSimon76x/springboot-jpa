e2d06c8ad57ee5533549701080a459871ace79a0
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-08-08 10:05:44 -0400

    Consultas personalizadas con anotación Query y nomenclatura Query Methods

--- src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -22,8 +22,10 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Person> persons = (List<Person>) repository.findAll();
-
+		// List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("JavaScript");
+		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("Java", "Andres");
+		
 		persons.stream().forEach(person -> System.out.println(person));
 	}
 
--- src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/simon/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,9 +1,18 @@
 package com.simon.curso.springboot.jpa.springboot_jpa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.simon.curso.springboot.jpa.springboot_jpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
 
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+    
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
+    
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
 }
